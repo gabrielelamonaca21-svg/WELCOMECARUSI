7a22d4122577d8e16cff713f81a29bc67fd472eb
gabrielelamonaca21-svg <gabrielelamonaca21@gmail.com>
2026-06-02 06:03:07 +0200

    Update index.js

--- welcome/welcome/index.js
+++ welcome/welcome/index.js
@@ -1,1 +1,31 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
+// Creiamo una nuova istanza del bot con i permessi necessari
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers, // FONDAMENTALE per rilevare i nuovi utenti!
+    ],
+});
+
+// --- CODE HANDLER PER GLI EVENTI ---
+// Questo pezzo di codice legge in automatico tutti i file dentro la cartella /events
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
+    }
+}
+
+// Avviamo il bot usando il token nel config.json
+client.login(token);
